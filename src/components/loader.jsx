1599642d94310a2bdcc0a0fd03de58a61dd516d8
Geeth2000 <geethanjankaru123@gmail.com>
2025-12-14 16:17:19 +0530

    Rename Loader component function to lowercase and adjust export statement

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,7 +1,11 @@
-export default function Loader() {
+import React from "react";
+
+function loader() {
   return (
     <div className="w-full h-screen fixed top-0 left-0 bg-black/45 flex justify-center items-center">
       <div className="w-[100px] h-[100px] border-4 border-accent border-t-transparent rounded-full animate-spin "></div>
     </div>
   );
 }
+
+export default loader;
